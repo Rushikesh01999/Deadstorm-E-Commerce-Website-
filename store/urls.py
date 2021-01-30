e1260b46cb18d5435062504937038e0f135b833f
rushikesh tummod <rushikesh01999@gmail.com>
2021-01-30 13:39:34 +0530

    first commit

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+	path('',views.store,name='store'),
+	path('cart/',views.cart,name='cart'),
+	path('checkout/',views.checkout,name='checkout'),
+	path('update_item/',views.updateItem,name='update'),
+	path('process_order/',views.processOrder,name='process_order')
+]
